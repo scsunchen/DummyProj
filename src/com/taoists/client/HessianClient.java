7c8f9f5763bdfbac666feb7c2eee0f32f6b1cb2c
Lv, Simon <simlv@airbiquity.com>
2013-10-29 11:31:55 +0800

    29OCT2013 Do research gfor Hessian functionality

--- src/com/taoists/client/HessianClient.java
+++ src/com/taoists/client/HessianClient.java
@@ -13,13 +13,26 @@ import com.taoists.hessian.HelloService;
 public class HessianClient {
 
 	public static void main(String[] args) throws Exception {
-		ApplicationContext ctx = new ClassPathXmlApplicationContext("hessian-client.xml");
+
+		ApplicationContext ctx = new ClassPathXmlApplicationContext(
+				"hessian-client.xml");
 
 		HelloService hs = (HelloService) ctx.getBean("helloService");
 		p(hs.hashCode());
 
+		// Hessian服务的url
+		// String url =
+		// "http://localhost:8080/DummyProj/services/hello-service";
+		// p("url ==> " + url);
+		// HessianProxyFactory factory = new HessianProxyFactory();
+
+		// 获得Hessian服务的远程引用
+		// HelloService hs = (HelloService) factory
+		// .create(HelloService.class, url);
+
+		hs.hello();
 		for (Demo demo : hs.getDemos()) {
-			p(demo.getCode() + "<>" + demo.getName());
+			p(demo.getCode() + " <> " + demo.getName());
 		}
 	}
 
--- src/com/taoists/hessian/impl/HelloServiceImpl.java
+++ src/com/taoists/hessian/impl/HelloServiceImpl.java
@@ -2,10 +2,11 @@ package com.taoists.hessian.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.google.common.collect.Lists;
 import com.taoists.demo.entity.Demo;
+import com.taoists.demo.service.DemoService;
 import com.taoists.hessian.HelloService;
 
 /**
@@ -14,17 +15,21 @@ import com.taoists.hessian.HelloService;
  */
 @Service("helloService")
 public class HelloServiceImpl implements HelloService {
-
+	@Autowired
+	protected DemoService demoService;
+	
 	public String hello() {
 		return "This is the hello";
 	}
 
 	@Override
 	public List<Demo> getDemos() {
-		Demo demo = new Demo();
+		/*Demo demo = new Demo();
 		demo.setCode("10000");
 		demo.setName("This is test");
-		return Lists.newArrayList(demo);
+		return Lists.newArrayList(demo);*/
+		
+		return demoService.findAll();
 	}
 
 }
